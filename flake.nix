c192292e220354af3d3465e48f855e578666781a
rw <rw@jjk.is>
2024-02-13 19:31:36 -0800

    build

--- flake.nix
+++ flake.nix
@@ -1,11 +1,35 @@
 {
-  description = "A very basic flake";
+  description = "A basic rust cli";
 
-  outputs = { self, nixpkgs }: {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    (flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
 
-  };
+          app = pkgs.rustPlatform.buildRustPackage {
+            pname = "app";
+            version = "0.0.1";
+            src = ./.;
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+          };
+        in
+        {
+          app = app;
+          packages.default = app;
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        })
+    );
 }
